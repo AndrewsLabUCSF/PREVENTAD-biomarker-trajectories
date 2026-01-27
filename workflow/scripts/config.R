8efe3eda2130f076c8393f938a186dba5bd8bd87
Michelle Li <michelle.li2@ucsf.edu>
2026-01-27 14:28:23 -0800

    Removed BrANCH from config file

--- workflow/scripts/config.R
+++ workflow/scripts/config.R
@@ -15,16 +15,15 @@ PLOT_FN <- file.path(FUNCTIONS_PATH, "functions_plot.R")
 
 # BIOMARKER NAMES ---------------------------------------------------------
 BIOMARKER_VARS <- c("AB_ratio_simoa_4plex", "AB42_simoa_4plex", "GFAP_simoa_4plex", "NFL_simoa_4plex",
-                    "ptau181_simoa_UGOT", "ptau217_simoa_UGOT", "ptau231_simoa_UGOT")
-BIOMARKERS <- c("ab_ratio", "gfap", "nfl", "ptau181", "ptau217", "ptau231",
-                "ptau217_ab42_ratio")
+                    "ptau181_simoa_UGOT", "ptau217_simoa_UGOT")
+BIOMARKERS <- c("ab_ratio", "gfap", "nfl", 
+                "ptau181", "ptau217", "ptau217_ab42_ratio")
 BIOMARKERS_TRANS <- c("ab_ratio", "gfap_log", "nfl_log", "ptau181_log", 
-                      "ptau217_log", "ptau231_sqrt", "ptau217_ab42_ratio_log")
+                      "ptau217_log", "ptau217_ab42_ratio_log")
 
 
 # INPUT DATA PATH ---------------------------------------------------------
 RAW_PREVENTAD_DATA_PATH <- file.path("~/Library/CloudStorage/Box-Box/AndrewsLab/data/PREVENT-AD/data/")
-RAW_BRANCH_DATA_PATH <- file.path("~/Library/CloudStorage/Box-Box/AndrewsLab/data/BrANCH")
 
 
 # OUTPUT DATA PATHS -------------------------------------------------------
