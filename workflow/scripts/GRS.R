39299b7184d11341b5537376567a910d1ab5a815
Michelle Li <michelle.li2@ucsf.edu>
2026-04-13 13:35:20 -0700

    Fixed GRS script

--- workflow/scripts/GRS.R
+++ workflow/scripts/GRS.R
@@ -13,7 +13,7 @@ bellenguez_results <- read_xlsx("data/raw/41588_2022_1024_MOESM4_ESM.xlsx",
 gwas <- gwas_raw
 
 GRSweights <- data.frame(
-  snp = paste0(bellenguez_results$rsid, "_", bellenguez_results$`Risk allele`), 
+  rsid = bellenguez_results$rsid,
   beta = bellenguez_results$Beta,
   effect_allele = bellenguez_results$`Risk allele`,
   pval = bellenguez_results$`P value`
@@ -24,12 +24,21 @@ GRSweights <- data.frame(
 # GRS CALCULATION ---------------------------------------------------------
 # Function for GRS calculation
 calculate_grs <- function(geno_matrix, weights_df) {
-  # Ensure SNPs are in same order
-  common_snps <- intersect(colnames(geno_matrix), weights_df$snp)
+  # Parse rsid and allele from column names (format: rsid_allele)
+  geno_cols   <- colnames(geno_matrix)
+  geno_rsid   <- sub("_[^_]+$", "", geno_cols)
+  geno_allele <- sub(".*_", "", geno_cols)
   
-  # Subset and reorder
-  geno_subset <- geno_matrix[, common_snps, drop = FALSE]
-  weights_subset <- weights_df[match(common_snps, weights_df$snp), ]
+  # Match on rsid
+  weights_idx <- match(geno_rsid, weights_df$rsid)
+  matched     <- !is.na(weights_idx)
+  
+  geno_subset    <- geno_matrix[, matched, drop = FALSE]
+  weights_subset <- weights_df[weights_idx[matched], ]
+  
+  # Flip dosage where PREVENT-AD allele != Bellenguez risk allele
+  needs_flip <- geno_allele[matched] != weights_subset$effect_allele
+  geno_subset[, needs_flip] <- 2L - geno_subset[, needs_flip]
   
   # Calculate GRS as matrix multiplication
   grs <- as.numeric(geno_subset %*% weights_subset$beta)
