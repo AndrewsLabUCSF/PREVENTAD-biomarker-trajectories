dc78eb37caf743d4d4e1adab8d48cce8c5c1d619
Michelle Li <michelle.li2@ucsf.edu>
2026-04-14 17:40:51 -0700

    Moved risk burden to loading script

--- workflow/analysis/00_load_analysis_data.R
+++ workflow/analysis/00_load_analysis_data.R
@@ -11,7 +11,6 @@ fhx_dat <- readRDS(file.path(DATA_INTERMEDIATE_PATH, "PREVENTAD_fhx.rds"))
 grs_dat <- readRDS(file.path(DATA_INTERMEDIATE_PATH, "PREVENTAD_GRS.rds"))
 mci_dat <- readRDS(file.path(DATA_INTERMEDIATE_PATH, "PREVENTAD_MCI.rds"))
 
-
 # Load CRS factors and scored risk scores
 crs_factors <- readRDS(file.path(DATA_INTERMEDIATE_PATH, "PREVENTAD_crsfactors_imputed.rds"))
 cogd_scored <- readRDS(file.path(DATA_INTERMEDIATE_PATH, "PREVENTAD_cogd_scored.rds"))
@@ -27,7 +26,24 @@ predictors_dat <- crs_factors %>%
   left_join((fhx_dat %>% select(CONP_ID, family_history)), by = "CONP_ID") %>%
   left_join((cogd_scored %>% select(CONP_ID, score_cogd)), by = "CONP_ID") %>%
   left_join((libra_scored %>% select(CONP_ID, score_libra2)), by = "CONP_ID") %>%
-  select(CONP_ID, age, sex, educ, apoe, score_cogd, GRS, score_libra2, family_history)
+  select(CONP_ID, age, sex, educ, apoe, score_cogd, GRS, score_libra2, family_history) %>%
+  # Risk burden components: 
+  mutate(
+    grs_quartile      = ntile(GRS, 4),
+    grs_burden        = if_else(grs_quartile == 4, 1, 0),
+    fhx_burden        = if_else(family_history == "1 FDR", 0, 1),
+    apoe_burden       = if_else(apoe == "e4+", 1, 0),
+    
+    # CRS cutoff strategy: 1 SD above mean (selected via aim1_crs_cutoffs analysis)
+    cogdrisk_burden   = if_else(score_cogd > mean(score_cogd, na.rm = TRUE) +
+                                  sd(score_cogd, na.rm = TRUE), 1, 0),
+    libra2_burden     = if_else(score_libra2 > mean(score_libra2, na.rm = TRUE) +
+                                  sd(score_libra2, na.rm = TRUE), 1, 0),
+    risk_score_cogd   = rowSums(across(c(fhx_burden, apoe_burden,
+                                         cogdrisk_burden, grs_burden)), na.rm = TRUE),
+    risk_score_libra2 = rowSums(across(c(fhx_burden, apoe_burden,
+                                         libra2_burden, grs_burden)), na.rm = TRUE)
+  )
 
 # Save predictors dataframe
 saveRDS(predictors_dat, file.path(DATA_CLEANED_PATH, "PREVENTAD_predictors.rds"))
@@ -52,7 +68,7 @@ other_biomarkers_first <- biomarkers %>%
   slice_head() %>%
   ungroup()
 
-biomarker_baseline_dat <- predictors_dat %>%
+baseline_dat <- predictors_dat %>%
   left_join((ptau217_first %>% select(CONP_ID, ptau217)), by = "CONP_ID") %>%
   left_join(other_biomarkers_first, by = "CONP_ID") %>%
   # Add log transformations
@@ -82,11 +98,11 @@ other_biomarkers_last <- biomarkers %>%
   slice_tail() %>%
   ungroup()
 
-biomarker_last_dat <- predictors_dat %>%
+lastvisit_dat <- predictors_dat %>%
   left_join((ptau217_last %>% select(CONP_ID, ptau217)), by = "CONP_ID") %>%
   left_join(other_biomarkers_last, by = "CONP_ID") %>%
-  # Add log transformations
   mutate(
+    # Add log transformations
     gfap_log = log(gfap),
     nfl_log = log(nfl),
     ptau181_log = log(ptau181),
@@ -95,27 +111,33 @@ biomarker_last_dat <- predictors_dat %>%
   )
 
 # Filter out participants that progressed to MCI
-biomarker_last_nomci_dat <- biomarker_last_dat %>% filter(!CONP_ID %in% mci_dat$CONP_ID)
+lastvisit_nomci_dat <- lastvisit_dat %>% filter(!CONP_ID %in% mci_dat$CONP_ID)
+
 
 ## Longitudinal ----
+mean_years <- mean(biomarkers$years, na.rm = TRUE)
+
 all_dat <- predictors_dat %>%
   left_join(biomarkers, by = "CONP_ID") %>%
-  # Add log transformations
   mutate(
+    # Add log transformations
     gfap_log = log(gfap),
     nfl_log = log(nfl),
     ptau181_log = log(ptau181),
     ptau217_log = log(ptau217),
-    ptau217_ab42_ratio_log = log(ptau217_ab42_ratio)
+    ptau217_ab42_ratio_log = log(ptau217_ab42_ratio),
+    
+    # Grand-mean centering time around the average follow-up time across all observations
+    years_centered  = years - mean_years
   ) %>%
   select(-age.y) %>%
   rename(age = age.x)
 
 
 # SAVE FINAL DATAFRAMES ---------------------------------------------------
-saveRDS(biomarker_baseline_dat, file.path(DATA_CLEANED_PATH, "PREVENTAD_baseline.rds"))
-saveRDS(biomarker_last_dat, file.path(DATA_CLEANED_PATH, "PREVENTAD_lastvisit_all.rds"))
-saveRDS(biomarker_last_nomci_dat, file.path(DATA_CLEANED_PATH, "PREVENTAD_lastvisit_nomci.rds"))
+saveRDS(baseline_dat, file.path(DATA_CLEANED_PATH, "PREVENTAD_baseline.rds"))
+saveRDS(lastvisit_dat, file.path(DATA_CLEANED_PATH, "PREVENTAD_lastvisit_all.rds"))
+saveRDS(lastvisit_nomci_dat, file.path(DATA_CLEANED_PATH, "PREVENTAD_lastvisit_nomci.rds"))
 saveRDS(all_dat, file.path(DATA_CLEANED_PATH, "PREVENTAD_longitudinal.rds"))
 
 # Print summary
